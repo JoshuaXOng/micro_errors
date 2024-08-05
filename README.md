92836d5fbe2fad45838e4c54377dd48b62078392
Joshua Ong <joshuaong2000@gmail.com>
2024-08-05 23:25:06 +1000

    Unified linking api

--- README.md
+++ README.md
@@ -2,80 +2,47 @@
 
 Utilities to deal with errors without taking the derive macro approach.
 
-Exposes a struct `ErrorChain` to typically be used within the `Err` variant of `Result`. `ErrorChain` contains a generic payload and a recusive struct, `ErrorLink`, which in turn contains either a summary message or a backtrace (intended to be of the last link). See below.
-
-```rust
-#[derive(Debug)]
-pub struct ErrorChain<T: Display>(pub T, pub ErrorLink);
-```
+Exposes a struct `ErrorLink_` to typically be used within the `Err` variant of `Result`. It contains a generic payload, and further `ErrorLink_`s whose payloads are `String`s. The final link will point to `Backtrace` instead. The `Backtrace` is approximate, especially when linking from a non `ErrorLink_`. Also, remember to set `RUST_BACKTRACE`.
 
 For the generic payload as `String`, utilities have been implemented to aid chaining (e.g., `.map_err`'ing).
 
 ## Gist
 
-Creating an `ErrorChain` from a non `ErrorChain`.
+Creating an `ErrorLink_` from a non `ErrorLink_`.
 
 ```rust
-pub fn function_1() -> Result<(), ErrorChain<String>> {
-    Err::<(), _>(std::io::Error::other("not an `ErrorChain`"))
-        .map_err(ErrorChain::onboard_fn("that is ok"))
+use crate::ErrorLinkable;
+pub fn function() -> Result<(), ErrorLink_<String>> {
+    Err::<(), _>(std::io::Error::other("Underlying error."))
+        .map_err(std::io::Error::link_fn("Higher level error."))
 }
 ```
 
-Creating an `ErrorChain` from an existing `ErrorChain`.
+Creating an `ErrorLink_` from an existing `ErrorLink_`.
 
 ```rust
-pub fn function_2() -> Result<(), ErrorChain<String>> {
-    Err::<(), _>(ErrorChain::start("key glock"))
-        .map_err(ErrorChain::add_fn("who I be"))
-        .map_err(ErrorChain::add_fn("I dunno"))
+pub fn function() -> Result<(), ErrorLink_<String>> {
+    Err::<(), _>(ErrorLink_::new_string("Underlying error."))
+        .map_err(ErrorLink_::link_fn("Higher level error."))
 }
 ```
 
 Output of displaying/`println!`ing the error being something like below.
 
 ```
-Error no. 0: I dunno
-Error no. 1: who I be
-Error no. 2: key glock
-Approximate backtrace of error no. 2:
-   0: micro_errors::ErrorLink::severed
-             at ./src/lib.rs:58:23
-   1: micro_errors::ErrorChain<alloc::string::String>::start
-             at ./src/lib.rs:76:13
-   2: micro_errors::test_crate
-             at ./src/lib.rs:14:22
-   3: micro_errors::test_crate::{{closure}}
-             at ./src/lib.rs:4:16
-```
-
-Again, creating an `ErrorChain` from an existing `ErrorChain`.
-
-```rust
-#[allow(dead_code)]
-#[derive(PartialEq)]
-enum ErrorReasons {
-    One,
-    Two,
-    Three
-}
-impl std::fmt::Display for ErrorReasons {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let as_string = match self {
-            ErrorReasons::One => "one",
-            ErrorReasons::Two => "two (easter egg)",
-            ErrorReasons::Three => "three",
-        }; 
-        write!(f, "{}", as_string)
-    }
-}
-pub fn function_3() -> Result<(), ErrorChain<String>> {
-    match Err::<(), _>(ErrorChain(ErrorReasons::Two, ErrorLink::severed())) {
-        Err(error_chain) if error_chain.0 == ErrorReasons::Two => println!("{}", error_chain),
-        _ => panic!("look above"),
-    }
-
-    Err::<(), _>(ErrorChain(ErrorReasons::One, ErrorLink::severed()))
-        .map_err(|e| ErrorChain::add("food", e))
-}
+Error no. 0: Higher level error.
+Error no. 1: Underlying error.
+Approximate backtrace of error no. 1:
+   0: micro_errors::ErrorLink_<alloc::string::String>::new_string
+             at ./src/lib.rs:270:51
+   1: micro_errors::tests::test__chaining_error_link_
+             at ./src/lib.rs:72:26
+   2: micro_errors::tests::test__chaining_error_link_::{{closure}}
+             at ./src/lib.rs:69:36
+   3: core::ops::function::FnOnce::call_once
+             at /rustc/051478957371ee0084a7c0913941d2a8c4757bb9/library/core/src/ops/function.rs:250:5
+   4: core::ops::function::FnOnce::call_once
+             at /rustc/051478957371ee0084a7c0913941d2a8c4757bb9/library/core/src/ops/function.rs:250:5
+   5: test::__rust_begin_short_backtrace
+             at /rustc/051478957371ee0084a7c0913941d2a8c4757bb9/library/test/src/lib.rs:625:18
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,164 @@
-use std::{backtrace::Backtrace, fmt::{Display, Debug}};
+use std::{any::Any, backtrace::Backtrace, fmt::{Debug, Display}};
 
-#[test]
-fn test_crate() {
-    println!(
-        "{:#?}", 
-        Err::<(), _>(std::io::Error::other("not an `ErrorChain`"))
-            .map_err(ErrorChain::onboard_fn("that is ok"))
-            .expect_err("look above")
-    );
-
-    println!(
-        "{}", 
-        Err::<(), _>(ErrorChain::start("key glock"))
-            .map_err(ErrorChain::add_fn("who I be"))
-            .map_err(ErrorChain::add_fn("I dunno"))
-            .expect_err("look above")
-    );
+#[cfg(test)]
+mod tests {
+    #[allow(deprecated)]
+    use crate::ErrorChain;
+    use crate::ErrorLink_;
+    use crate::ErrorLinkable;
+    use crate::NextLink;
+    use std::backtrace::Backtrace;
 
     #[allow(dead_code)]
-    #[derive(PartialEq)]
+    trait X {
+        fn letter() -> char;
+    }
+    impl<T> X for T {
+        fn letter() -> char {
+            'X'
+        }
+    }
+    struct B;
+    impl B {
+        fn letter() -> char {
+            'b'
+        }
+    }
+
+    #[test]
+    fn test_favour_concrete() {
+        assert!(B::letter() == 'b');
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    #[allow(deprecated)]
+    fn test__chaining_non_error_chain() {
+        println!(
+            "{}", 
+            Err::<(), _>(std::io::Error::other("Underlying error."))
+                .map_err(ErrorChain::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn test__chaining_non_error_link_() {
+        println!(
+            "{}", 
+            Err::<(), _>(std::io::Error::other("Underlying error."))
+                .map_err(std::io::Error::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+    }
+
+    #[test]
+    #[allow(deprecated)]
+    #[allow(non_snake_case)]
+    fn test__chaining_error_chain() {
+        println!(
+            "{}", 
+            Err::<(), _>(ErrorChain::start("Underlying error."))
+                .map_err(ErrorChain::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test__chaining_error_link_() {
+        println!(
+            "{}", 
+            Err::<(), _>(ErrorLink_::new_string("Underlying error."))
+                .map_err(ErrorLink_::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+    }
+
+    impl ErrorLink_<i32> {
+        pub fn new_i32(error_number: impl Into<i32>) -> Self {
+            Self(error_number.into(), NextLink::None(Backtrace::capture()))
+        }
+    }
+    #[derive(Debug, PartialEq)]
     enum ErrorReasons {
         One,
         Two,
+        #[allow(dead_code)]
         Three
+    }
+    impl ErrorLink_<ErrorReasons> {
+        pub fn new_reason(error_reason: ErrorReasons) -> Self {
+            Self(error_reason, NextLink::None(Backtrace::capture()))
+        }
     }
     impl std::fmt::Display for ErrorReasons {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
             let as_string = match self {
-                ErrorReasons::One => "one",
-                ErrorReasons::Two => "two (easter egg)",
-                ErrorReasons::Three => "three",
+                ErrorReasons::One => "First reason for underlying error.",
+                ErrorReasons::Two => "Second reason for underlying error.",
+                ErrorReasons::Three => "Third reason for underlying error.",
             }; 
             write!(f, "{}", as_string)
         }
     }
-    println!(
-        "{}", 
-        Err::<(), _>(ErrorChain(ErrorReasons::One, ErrorLink::severed()))
-            .map_err(|e| ErrorChain::add("food", e))
-            .expect_err("look above")
-    );
+    impl std::error::Error for ErrorReasons {}
 
-    match Err::<(), _>(ErrorChain(ErrorReasons::Two, ErrorLink::severed())) {
-        Err(error_chain) if error_chain.0 == ErrorReasons::Two => println!("{}", error_chain),
-        _ => panic!("look above"),
+    #[test]
+    #[allow(non_snake_case)]
+    fn test__chaining_error_link__non_string_payload() {
+        println!(
+            "{}", 
+            Err::<(), _>(ErrorLink_::new_i32(100))
+                .map_err(ErrorLink_::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+
+        println!(
+            "{}", 
+            Err::<(), _>(ErrorLink_::new_reason(ErrorReasons::One))
+                .map_err(ErrorLink_::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
+
+        match Err::<(), _>(ErrorLink_::new_reason(ErrorReasons::Two)) {
+            Err(error_chain) if error_chain.0 == ErrorReasons::Two => println!("{}", error_chain),
+            _ => panic!("look above"),
+        }
+    }
+
+    #[test]
+    #[allow(deprecated)]
+    #[allow(non_snake_case)]
+    fn test__chaining_non_error_chain_and_non_error_trait() {
+        println!(
+            "{}", 
+            Err::<(), _>(String::from("Underlying error."))
+                .map_err(ErrorChain::onboard_fn("Higher level error."))
+                .expect_err("look above")
+        );
+
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn test__chaining_non_error_link_and_non_error_trait() {
+        println!(
+            "{}", 
+            Err::<(), _>(String::from("Underlying error."))
+                .map_err(String::link_fn("Higher level error."))
+                .expect_err("look above")
+        );
     }
 }
 
 #[derive(Debug)]
+#[deprecated(since = "0.3.0", note="use `NextLink` instead")]
 pub enum ErrorLink {
     Severed(Backtrace),
     Continued(String, Box<ErrorLink>)
 }
 
+#[allow(deprecated)]
 impl ErrorLink {
     pub fn severed() -> Self {
         Self::Severed(Backtrace::capture())
@@ -67,14 +173,21 @@ impl ErrorLink {
 }
 
 #[derive(Debug)]
+#[deprecated(since = "0.3.0", note="use `ErrorLink_` instead")]
+#[allow(deprecated)]
 pub struct ErrorChain<T: Display>(pub T, pub ErrorLink);
 
+#[allow(deprecated)]
+impl<T: std::error::Error> From<T> for ErrorChain<String> {
+    fn from(value: T) -> Self {
+        ErrorChain(value.to_string(), ErrorLink::severed())
+    }
+}
+
+#[allow(deprecated)]
 impl ErrorChain<String> {
     pub fn start(error_message: impl Into<String>) -> Self {
-        Self(
-            error_message.into(), 
-            ErrorLink::severed()
-        )
+        Self(error_message.into(), ErrorLink::severed())
     }
 
     pub fn add<T: Display>(error_message: impl Into<String>, current_chain: ErrorChain<T>) -> Self {
@@ -85,10 +198,7 @@ impl ErrorChain<String> {
         move |current_chain| {
             Self(
                 error_message.into(),
-                ErrorLink::continued(
-                    current_chain.0.to_string(), 
-                    current_chain.1
-                )
+                ErrorLink::continued(current_chain.0.to_string(), current_chain.1)
             )
         }
     }
@@ -101,15 +211,27 @@ impl ErrorChain<String> {
         move |underlying_error| {
             Self(
                 error_message.into(),
-                ErrorLink::Continued(
-                    underlying_error.to_string(), 
-                    Box::new(ErrorLink::severed())
-                ) 
+                ErrorLink::continued(underlying_error.to_string(), Box::new(ErrorLink::severed())) 
+            )
+        }
+    }
+
+    pub fn link(error_message: impl Into<String>, current_chain: impl Into<Self>) -> Self {
+        ErrorChain::link_fn(error_message)(current_chain)
+    }
+
+    pub fn link_fn<T: Into<Self>>(error_message: impl Into<String>) -> impl FnOnce(T) -> Self {
+        move |current_chain| {
+            let current_chain = current_chain.into();
+            Self(
+                error_message.into(), 
+                ErrorLink::continued(current_chain.0, current_chain.1)
             )
         }
     }
 }
 
+#[allow(deprecated)]
 impl<T: std::fmt::Display> std::fmt::Display for ErrorChain<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Error no. 0: {}\n", self.0)?;
@@ -126,6 +248,68 @@ impl<T: std::fmt::Display> std::fmt::Display for ErrorChain<T> {
                 ErrorLink::Continued(error_message, error_link) => {
                     write!(f, "Error no. {error_number}: {error_message}\n")?;
                     error_link
+                },
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum NextLink {
+    None(Backtrace),
+    Some(Box<ErrorLink_<String>>)
+}
+
+#[derive(Debug)]
+pub struct ErrorLink_<Payload: Display>(pub Payload, pub NextLink);
+
+impl ErrorLink_<String> {
+    pub fn new_string(error_message: impl Into<String>) -> Self {
+        Self(error_message.into(), NextLink::None(Backtrace::capture()))
+    }
+
+    pub fn link_fn<T: Display>(error_message: impl Into<String>) -> impl FnOnce(ErrorLink_<T>) -> Self {
+        move |next_link| {
+            let next_link = Box::new(ErrorLink_(next_link.0.to_string(), next_link.1));
+            ErrorLink_(error_message.into(), NextLink::Some(next_link))
+        }
+    }
+}
+
+pub trait ErrorLinkable<T, Payload: Display>: Any + Display {
+    fn link_fn(error_message: impl Into<Payload>) -> impl FnOnce(T) -> ErrorLink_<Payload>;
+}
+
+impl<T: Any + Display> ErrorLinkable<T, String> for T {
+    fn link_fn(error_message: impl Into<String>) -> impl FnOnce(T) -> ErrorLink_<String> {
+        move |underlying_error| {
+            let next_link = Box::new(ErrorLink_(
+                String::from(underlying_error.to_string()), 
+                NextLink::None(Backtrace::capture())
+            ));
+            ErrorLink_(error_message.into(), NextLink::Some(next_link))
+        }
+    }
+}
+
+impl<T: Display> Display for ErrorLink_<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error no. 0: {}\n", self.0)?;
+        let mut next_link = &self.1;
+        for error_number in 1.. {
+            next_link = match next_link {
+                NextLink::None(end_backtrace) => {
+                    write!(
+                        f, "Approximate backtrace of error no. {}:\n{end_backtrace}", 
+                        error_number - 1
+                    )?;
+                    break;
+                }, 
+                NextLink::Some(error_link) => {
+                    write!(f, "Error no. {error_number}: {}\n", error_link.0)?;
+                    &error_link.1
                 },
             }
         }
